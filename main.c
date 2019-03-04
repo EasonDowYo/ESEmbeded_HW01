2b3e50e77ca4942f8b2f44aa9af1f260c3482256
EasonDowYo <king0906sk@gmail.com>
2019-03-04 20:31:10 +0800

    Using Array function pointer

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add(int a,int b){return a+b}
-int minus(int a,int b){return a-b}
-int divi(int a,int b){return a/b}
-int multi(int a,int b){return a*b}
+int add(int a,int b){return a+b;}
+int minus(int a,int b){return a-b;}
+int divi(int a,int b){return a/b;}
+int multi(int a,int b){return a*b;}
 int (*FunPtr[4])(int a,int b)={add,minus,divi,multi};
 int main(){
-    int case,a,b,result;
+    int option,a,b,result;
     
     printf("Enter function number you want\n");
     printf("You should enter case 0~3");
-    scanf("%d %d %d",&a,&b,%case);
+    scanf("%d %d %d",&a,&b,&option);
     
-    if((case>=0)&&(case<=3))
+    if((option>=0)&&(option<=3))
     {
-        result=(*FunPtr[case])(a,b);
+        result=(*FunPtr[option])(a,b);
     }    
     printf("a=%d , b=%d , result=%d",a,b,result);
     return 0;
